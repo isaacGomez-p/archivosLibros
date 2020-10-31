2d9c6ddc61caa4d22af9319c03a7bc7ea7b8a77e
isaacGomez-p <isaacgomez47@hotmail.com>
2020-10-31 15:06:08 -0500

    commit pru

--- src/main/java/com/movit/ArchivosLibrosApplication.java
+++ src/main/java/com/movit/ArchivosLibrosApplication.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("com.movit")
-@EntityScan("com.movit.entity")
-@EnableJpaRepositories("com.movit.repository")
+
 public class ArchivosLibrosApplication {
 
 	public static void main(String[] args) {
